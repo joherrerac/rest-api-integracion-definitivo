76db81e1eb263655cfd89643458bc25bf016b5b0
joherrerac <jo.herrerac@duocuc.cl>
2023-05-11 20:15:14 -0400

    termino rest api con observaciones

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, Response
 from flask_pymongo import PyMongo
+from bson import json_util
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = "mongodb+srv://joherrerac:<cotito2001j>@cluster0.dn5pzce.mongodb.net/?retryWrites=true&w=majority"
@@ -30,13 +32,54 @@ def create_products():
     else:
         return not_found()
 
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = mongo.db.users.find()
+    response = json_util.dumps(users)
+    
+    return Response(response, mimetype='application/json')
+
+@app.route('/users/<id>', methods=['GET'])
+def get_user(id):
+    user = mongo.db.users.find_one({"_id": ObjectId(id)})
+    response = json_util(user)
+    
+    return Response(response, mimetype='application/json')
+
+@app.route('/users/<id>', methods=['DELETE'])
+def delete_user(id):
+    mongo.db.users.delete_one({"_id": ObjectId(id)})
+    response = jsonify({"message": 'User' + id + 'was deleted succefully'})
+    
+    return response
+
+@app.route('/users/<_id>', methods=['PUT'])
+def update_user(_id):
+    nombre = request.json['nombre']
+    descripcion = request.json['descripcion']
+    categoria = request.json['categoria']
+    precio = request.json['precio']
+    stock = request.json['stock']
+    if nombre and descripcion and categoria and precio and stock:
+        mongo.db.users.update_one(
+            {'_id': ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)}, {'$set': {'nombre': nombre, 'descripcion': descripcion, 'categoria': categoria, 'precio': precio, 'stock': stock}})
+        response = jsonify({'message': 'User' + _id + 'Updated Successfuly'})
+        response.status_code = 200
+        return response
+    else:
+        return not_found()
+
+
+@app.errorhandler(404)
+
 @app.errorhandler(404)
 def not_found(error=None):
-    message = {
+    response = jsonify({
         'message': 'Resource not found: ' + request.url,
         'status': 404
-    }
-    return message
+    })
+    response.status_code = 404
+    return response
 
 if __name__ == "__main__":
     app.run(debug=True)
